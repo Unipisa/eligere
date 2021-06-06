2b5c852c064f17eb3accfb4795f591850f947b8d
Antonio Cisternino <cisterni@di.unipi.it>
2021-06-06 21:08:03 +0200

    Refactoring for abstracting OTP management

---
+++ src/EligereES/OTPSender.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EligereES
+{
+    public static class OTPSender
+    {
+        public static string GenerateOTP()
+        {
+            var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var rnd = new Random();
+            var ret = new System.Text.StringBuilder();
+            for (var i = 0; i < 3; i++)
+            {
+                ret.Append(alphabet[rnd.Next(alphabet.Length)]);
+            }
+            return ret.ToString();
+        }
+
+        public static void SendMail(IConfiguration conf, string otp, string dest)
+        {
+            var smtp = new System.Net.Mail.SmtpClient("mixer.unipi.it", 587);
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential("statini", "bDYT5exqLzTwBp9");
+            smtp.Send("no-reply@unipi.it", dest, "Codice OTP per Eligere", $"Il codice OTP per procedere al voto è: {otp}");
+        }
+
+        public static void SendSMS(IConfiguration conf, string otp, string mobile)
+        {
+            var m = HttpUtility.UrlEncode(mobile);
+            var endpoint = conf.GetValue<string>("SendSMSEndPoint");
+            var req = WebRequest.Create(endpoint + "?msg=" + otp + "%20Codice%20di%20voto%20di%20Eligere&num=" + m);
+            var resp = req.GetResponse();
+        }
+    }
+}
