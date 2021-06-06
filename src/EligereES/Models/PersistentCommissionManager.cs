54d6caa33442ff070d2de836b84530125a8607ff
Antonio Cisternino <cisterni@di.unipi.it>
2021-06-06 21:07:33 +0200

    Model used to bookkeep commissioner identification workload

---
+++ src/EligereES/Models/PersistentCommissionManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EligereES.Models
+{
+    public class PersistentCommissionManager
+    {
+        private Random rnd = new Random();
+        private List<(Guid, DateTime)> BusyRemoteCommissioners = new List<(Guid, DateTime)>();
+
+        public TimeSpan Expiration { get; set; }
+
+        public Random Rnd { get { return rnd; } }
+
+        public void CollectExpiredItems()
+        {
+            lock (BusyRemoteCommissioners)
+            {
+                BusyRemoteCommissioners = BusyRemoteCommissioners.Where(p => p.Item2 >= DateTime.Now).ToList();
+            }
+        }
+
+        public List<Guid> GetBusyCommissioners()
+        {
+            return BusyRemoteCommissioners.ConvertAll(p => p.Item1);
+        }
+
+        public void AddBusyCommissioner(Guid id)
+        {
+            lock(BusyRemoteCommissioners)
+            {
+                BusyRemoteCommissioners.Add((id, DateTime.Now + Expiration));
+            }
+        }
+    }
+}
