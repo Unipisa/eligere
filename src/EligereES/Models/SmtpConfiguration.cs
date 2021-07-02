26b04e77761e53450fae65072515a9dfa5f3c0c2
Antonio Cisternino <cisterni@di.unipi.it>
2021-07-02 07:05:25 +0200

    SmtpConfiguration schema

---
+++ src/EligereES/Models/SmtpConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EligereES.Models
+{
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
