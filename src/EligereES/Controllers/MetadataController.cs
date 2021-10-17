39d4972440aa309e1fe0e33c6cb758e3e70fd8b2
ANTONIO CISTERNINO <n010223@ad.unipi.it>
2021-10-17 16:20:48 +0200

    Added files needed for the SAML authentication. The library ITfoxtec.Identity.Saml2.dll has been modified to force a URL. It should be fixed in future

---
+++ src/EligereES/Controllers/MetadataController.cs
@@ -0,0 +1,85 @@
+﻿using ITfoxtec.Identity.Saml2;
+using ITfoxtec.Identity.Saml2.MvcCore;
+using ITfoxtec.Identity.Saml2.Schemas;
+using ITfoxtec.Identity.Saml2.Schemas.Metadata;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EligereES.Controllers
+{
+    [AllowAnonymous]
+    [Route("Metadata")]
+    public class MetadataController : Controller
+    {
+        private readonly Saml2Configuration config;
+
+        public MetadataController(IOptions<Saml2Configuration> configAccessor)
+        {
+            config = configAccessor.Value;
+        }
+
+        public IActionResult Index()
+        {
+//            var defaultSite = new Uri($"{Request.Scheme}://{Request.Host.ToUriComponent()}/");
+            var defaultSite = new Uri($"https://eligere.unical.it/");
+
+            var entityDescriptor = new EntityDescriptor(config);
+            entityDescriptor.ValidUntil = 365;
+            entityDescriptor.SPSsoDescriptor = new SPSsoDescriptor
+            {
+                WantAssertionsSigned = true,
+                SigningCertificates = new X509Certificate2[]
+                {
+                    config.SigningCertificate
+                },
+                //EncryptionCertificates = new X509Certificate2[]
+                //{
+                //    config.DecryptionCertificate
+                //},
+                SingleLogoutServices = new SingleLogoutService[]
+                {
+                    new SingleLogoutService { Binding = ProtocolBindings.HttpPost, Location = new Uri(defaultSite, "Auth/SingleLogout"), ResponseLocation = new Uri(defaultSite, "Auth/LoggedOut") }
+                },
+                NameIDFormats = new Uri[] { NameIdentifierFormats.X509SubjectName },
+                AssertionConsumerServices = new AssertionConsumerService[]
+                {
+                    new AssertionConsumerService { Binding = ProtocolBindings.HttpPost, Location = new Uri(defaultSite, "Auth/AssertionConsumerService") },
+                    new AssertionConsumerService { Binding = ProtocolBindings.HttpPost, Location = new Uri(defaultSite, "Auth/AssertionConsumerService-test"), IsDefault = false }
+                },
+                AttributeConsumingServices = new AttributeConsumingService[]
+                {
+                    new AttributeConsumingService { ServiceName = new ServiceName("EligereSP", "en"), RequestedAttributes = CreateRequestedAttributes() }
+                },
+            };
+            entityDescriptor.ContactPersons = new[] { 
+                new ContactPerson(ContactTypes.Administrative)
+                {
+                    Company = "Università della Calabria",
+                    GivenName = "Giuseppe",
+                    SurName = "De Marco",
+                    EmailAddress = "mailto:idem-support@unical.it",
+                },
+                new ContactPerson(ContactTypes.Technical)
+                {
+                    Company = "Università della Calabria",
+                    GivenName = "Francesco",
+                    SurName = "Filicetti",
+                    EmailAddress = "mailto:idem-support@unical.it",
+                }
+            };
+            return new Saml2Metadata(entityDescriptor).CreateMetadata().ToActionResult();
+        }
+
+        private IEnumerable<RequestedAttribute> CreateRequestedAttributes()
+        {
+            yield return new RequestedAttribute("codice_fiscale");
+            yield return new RequestedAttribute("matricola_dipendente");
+            yield return new RequestedAttribute("sn");
+            yield return new RequestedAttribute("cn");
+        }
+    }
+}
---
+++ src/EligereES/Identity/ClaimsTransform.cs
@@ -0,0 +1,87 @@
+﻿using EligereES.Models;
+using EligereES.Models.DB;
+using ITfoxtec.Identity.Saml2.Claims;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+// From ITfoxtec WebCore example
+namespace EligereES.Saml.Identity
+{
+    public static class ClaimsTransform
+    {
+        public static ClaimsPrincipal Transform(IConfiguration conf, ClaimsPrincipal incomingPrincipal)
+        {
+            if (!incomingPrincipal.Identity.IsAuthenticated)
+            {
+                return incomingPrincipal;
+            }
+
+            return CreateClaimsPrincipal(conf, incomingPrincipal);
+        }
+
+        private static ClaimsPrincipal CreateClaimsPrincipal(IConfiguration conf, ClaimsPrincipal incomingPrincipal)
+        {
+            var claims = new List<Claim>();
+
+            // All claims
+            claims.AddRange(incomingPrincipal.Claims);
+            var opt = new DbContextOptionsBuilder<ESDB>();
+
+            using (var esdb = new ESDB(opt.UseSqlServer(conf.GetConnectionString("ESDB")).Options))
+            {
+                // Associate application roles
+                var rw = EligereRoles.ComputeRoles(esdb, "SAML2", incomingPrincipal.Identity.Name);
+                rw.Wait();
+                var roles = rw.Result;
+                roles.ForEach(r => claims.Add(new Claim(ClaimTypes.Role, r)));
+                var appIdentity = new ClaimsIdentity(claims, "EligereIdentity");
+                incomingPrincipal.AddIdentity(appIdentity);
+
+                // Trust the PublicID from the provider to associate UserLogin information
+                var cf = claims.Where(c => c.Type == "codice_fiscale").First().Value;
+                var p = esdb.Person.Where(p => p.PublicId == cf).FirstOrDefault();
+                if (p != null)
+                {
+                    if (!esdb.UserLogin.Where(u => u.UserId == incomingPrincipal.Identity.Name).Any())
+                    {
+                        var u = new UserLogin()
+                        {
+                            Id = System.Guid.NewGuid(),
+                            PersonFk = p.Id,
+                            Provider = "SAML2",
+                            UserId = incomingPrincipal.Identity.Name
+                        };
+                        esdb.UserLogin.Add(u);
+                        esdb.SaveChanges();
+                    }
+                }
+            }
+
+            return new ClaimsPrincipal(new ClaimsIdentity(claims, incomingPrincipal.Identity.AuthenticationType, ClaimTypes.NameIdentifier, ClaimTypes.Role)
+            {
+                BootstrapContext = ((ClaimsIdentity)incomingPrincipal.Identity).BootstrapContext
+            });
+        }
+
+        private static IEnumerable<Claim> GetSaml2LogoutClaims(ClaimsPrincipal principal)
+        {
+            yield return GetClaim(principal, Saml2ClaimTypes.NameId);
+            yield return GetClaim(principal, Saml2ClaimTypes.NameIdFormat);
+            yield return GetClaim(principal, Saml2ClaimTypes.SessionIndex);
+        }
+
+        private static Claim GetClaim(ClaimsPrincipal principal, string claimType)
+        {
+            return ((ClaimsIdentity)principal.Identity).Claims.Where(c => c.Type == claimType).FirstOrDefault();
+        }
+
+        private static string GetClaimValue(ClaimsPrincipal principal, string claimType)
+        {
+            var claim = GetClaim(principal, claimType);
+            return claim != null ? claim.Value : null;
+        }
+    }
+}
