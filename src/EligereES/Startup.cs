a8364593ac5f2d14e4eb2a260d168e054d8c5880
ANTONIO CISTERNINO <antonio.cisternino@unipi.it>
2022-05-20 11:34:29 +0200

    Fixed error in merge

--- src/EligereES/Startup.cs
+++ src/EligereES/Startup.cs
@@ -83,10 +83,8 @@ namespace EligereES
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            _logger = logger;
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
